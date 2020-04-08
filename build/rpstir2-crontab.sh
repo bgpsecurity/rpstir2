034c325868b4489ac416749cf36f887bbd191e27
cpusoft <cpusoft@live.com>
2020-04-08 13:44:22 +0800

    change sh

--- build/rpstir2-crontab.sh
+++ build/rpstir2-crontab.sh
@@ -6,7 +6,11 @@ run="$1"
 echo $run
 if [ $run == "rsync" ] ; then
 ./rpstir2-command.sh rsync
-else
+fi
+if [ $run == "start" ] ; then
 ./rpstir2-service.sh start
+fi
+if [ $run == "stop" ] ; then
+./rpstir2-service.sh stop
 fi
 echo -e "\n"
